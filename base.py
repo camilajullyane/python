305307a78e2e2b039f471ab1ff0daa15d639e11b
Camila Jullyane <134438133+camilajullyane@users.noreply.github.com>
2023-09-01 00:54:44 +0000

    asadasd

--- base.py
+++ base.py
@@ -1,21 +1,68 @@
-p1 = int(input('Qual o primeiro termo? '))
-r = int(input('Qual a razão da PA? '))
+onibus = {}
+cont = 0
+poltrona = []
 
-cont = 1
-while cont <= 10:
-    p1 += r
-    cont += 1
-    print(f'{p1 - r} -> ', end= '')
-print('PAUSA')
+
+def cadastro(cont):
+    onibus[f'passageiro {cont}'] = {
+        'nome' : input('Nome: '),
+        'rg' : input('RG: '),
+        'destino' : input('Destino: '),
+        'origem' : input('Origem: '),
+        'poltrona': verifica(cont)
+    }
+    poltrona.append(onibus[f'passageiro{cont}']['poltrona'])
+
+
+def lista():
+    for k,v  in onibus.items():
+        print(f'{k} : {v}')
+
+
+def modificar(p):
+    print('''
+        [1] Destino
+        [2] Origem
+        [3] Poltrona
+        [4] Sair
+''')
+    res2 = int(input('O que você deseja modificar? '))
+    match res2:
+        case 1:
+            onibus[f'passageiro{p}']['destino'] = input('Novo destino: ') 
+        case 2:
+            onibus[f'passageiro{p}']['origem'] = input('Nova origem: ')
+        case 3:
+            verifica(p)
+        case 4:
+            print('Saindo...')
+
+def verifica(p):
+        while True:
+            novapol = int(input('Nova poltrona: '))
+            if novapol in poltrona:
+                    print('Poltrona ocupada. Escolha outro lugar!')
+            else:
+                onibus[f'passageiro{p}']['poltrona'] = novapol
+                break
+
 while True:
-    res = int(input('Você quer ver mais quantos termos? '))
-    if res == 0:
-        print('Fim')
-        break
-    else:
-        cont = 1
-        while cont <= res:
-            p1 += r
+    print('''
+        [1] Cadastro
+        [2] Lista
+        [3] Modificar passagem
+        [4] Sair 
+''')
+    res = int(input('Escolha uma opção: '))
+    match res:
+        case 1:
             cont += 1
-            print(f'{p1 - r} -> ', end= '')
-        print('PAUSA')
+            cadastro(cont)
+        case 2:
+            lista()
+        case 3:
+            lista()
+            per = int(input('Qual passageiro você deseja modificar? '))
+            modificar(per)
+        case 4:
+            break
