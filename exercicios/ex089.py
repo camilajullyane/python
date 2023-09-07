70ae710ca3b2a146eeedaac7e6790a3bb8487b54
camilajullyane <camilaj.silva0@gmail.com>
2023-09-07 11:18:02 -0300

    att

--- exercicios/ex089.py
+++ exercicios/ex089.py
@@ -3,17 +3,30 @@ def validar(texto):
     while res not in 'SN':
         res = str(input(texto)).upper
     return res
-# matriz = []
-matriz = []
+
+
+alunos = []
+dados = []
 
 while True:
-    nome = str(input('Nome: '))
-    nota1 = float(input('Nota 1: '))
-    nota2 = float(input('Nota 2: '))
-    for c in matriz:
-
+    dados.append(input('Nome: '))
+    dados.append([float(input('Nota 1: ')), float(input('Nota 2: '))])
+    media = (dados[1][0] + dados[1][1]) / 2
+    dados.append(media)
+    alunos.append(dados[:])
+    dados.clear()
     res = validar('[ERRO DE ENTRADA]. Quer continuar? [S/N] ')
     if res == 'N':
         break
     
-matriz2 = [['Camila', [8, 8]] ]
+
+print(f'{"No":<4}{"NOME":<10}{"MÉDIA":>8}')
+for c, v in enumerate(alunos):
+    print(f'{c:<4}{v[0]:<10}{v[2]:>8}')
+
+while True:
+    pergunta = int(input('Mostrar notas de qual aluno: (999 interrompe) '))
+    if pergunta == 999:
+        break
+    print(f'Notas de {alunos[pergunta][0]} são: {alunos[pergunta][1]}')
+    
