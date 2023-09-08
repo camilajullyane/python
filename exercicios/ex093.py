5eb9136dd4a46fe7d04420145bab7fb1969980ec
camilajullyane <camilaj.silva0@gmail.com>
2023-09-07 21:20:06 -0300

    att

---
+++ exercicios/ex093.py
@@ -0,0 +1,24 @@
+pontuacao = []
+total = 0
+dicionario = {
+    'nome': input('Nome do jogador: ')
+    
+}
+
+partidas = int(input(f'Quantas partidas {dicionario["nome"]} jogou? '))
+for c in range(partidas):
+    gols = int(input(f'Quantos gols na partida {c}? '))
+    total += gols
+    pontuacao.append(gols)
+dicionario['gols'] = pontuacao[:]
+dicionario['total'] = total
+
+print('-='*30)
+for k, v in dicionario.items():
+    print(f'O campo {k} tem o valor de {v}')
+print('-='*30)
+
+print(f'O jogar {dicionario["nome"]} jogou {partidas}')
+for p, v in enumerate(pontuacao):
+    print(f'Na partida {p}, fez {v} gols')
+print(total)
